f671da93ed9f10669437faadd0eec5dbf876c247
Gustavo Rosa <gustavorib.rosa@gmail.com>
2023-09-28 16:13:18 -0300

    create tables

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,93 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dateOfBirth;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, int age, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student(String name, String email, int age, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
