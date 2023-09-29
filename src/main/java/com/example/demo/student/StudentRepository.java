b4713e3e4583bf869d6f81cae31150ca86c5a2ff
Gustavo Rosa <gustavorib.rosa@gmail.com>
2023-09-29 00:17:52 -0300

    POST implemented for student

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // first parameter is which type of object this repo will work with
 // second is the ID for the type
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+//    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
